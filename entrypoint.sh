38cdf123e2e48564832be5953d7b98ca6a45f1e6
tobe <tobeg3oogle@gmail.com>
2015-07-30 15:32:48 +0800

    Entrypoint support aufs environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
-/usr/bin/dockerlaunch /usr/bin/docker -d -s overlay &
+# Use overlay by defulat, if set AUFS=true just use aufs
+if [ -z "$AUFS" ]; then
+    echo "Use defualt overlay filesystem"
+    /usr/bin/dockerlaunch /usr/bin/docker -d -s overlay &
+else
+    echo "Use aufs filesystem"
+    /usr/bin/dockerlaunch /usr/bin/docker -d -s aufs &
+fi
 
+sleep 5
 /go/src/github.com/ArchCI/simple-worker/simple-worker
